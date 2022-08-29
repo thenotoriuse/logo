366460fbeb2e82ccecad27f9489090ec4c2b5a5b
widjesh <widjesh@satyamholidays.com>
2022-08-29 10:06:08 -0400

    Fixed faker

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,10 +1,11 @@
 import React from "react";
-import faker from "faker"
+import {faker} from '@faker-js/faker'
 const CommentDetail = pro => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" scr={faker.image.avatar()}/>
+          {console.log(faker.image.avatar())}
+        <img alt="avatar" src={faker.image.avatar()}/>
       </a>
       <div className="content">
         <a href="/" className="author">
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
+
 import CommentDetail from "./CommentDetail"
 const App = () => {
   return (
